6b28191ebf3c7f5615e43e96f5febbc2d027825d
Trmingnw <minhnthe161666@fpt.edu.vn>
2024-01-25 17:38:40 +0700

    model warehouse and note

---
+++ be/src/models/exportimportNote.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const { String, Number, ObjectId, Date } = mongoose.Schema.Types;
+
+const ExportImportNote = mongoose.model(
+  "ExportImportNote",
+  new mongoose.Schema(
+    {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+      },
+      userId: {
+        type: String, // You might want to use ObjectId here if it represents another user's ID
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+      price: {
+        type: Number,
+        required: true,
+      },
+      status: {
+        type: String,
+        enum: ["Pending", "Approved", "Rejected"], // Add relevant status values
+        default: "Pending",
+      },
+      description: {
+        type: String,
+      },
+      image: {
+        type: String, // You might want to use a different type if storing images, like Buffer or another library specific to handling images
+      },
+     
+    },
+    { timestamps: true }
+  )
+);
+
+export default ExportImportNote;
