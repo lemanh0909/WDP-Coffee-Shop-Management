2d514d9e4d5dfd6894e22845ec611daedba2315e
Trmingnw <minhnthe161666@fpt.edu.vn>
2024-02-12 22:13:09 +0700

    CRUD category

--- be/src/services/index.js
+++ be/src/services/index.js
@@ -3,4 +3,4 @@ export * from "./auth.service.js";
 export * from "./User.service.js";
 export * from "./staff.service.js";
 export * from "./warehouse.service.js";
-
+export * from "./category.service.js";
