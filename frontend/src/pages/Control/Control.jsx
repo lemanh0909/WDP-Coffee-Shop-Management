13e3efcda04c9b78382c7022facad5c2b9e5bb26
AnhTuannn1 <jackienguyennn01@gmail.com>
2024-02-23 13:15:45 +0700

    FE control screen

--- frontend/src/pages/Control/Control.jsx
+++ frontend/src/pages/Control/Control.jsx
@@ -13,6 +13,7 @@ import {
   InputGroup,
   FormControl,
   NavDropdown,
+  Image,
 } from "react-bootstrap";
 import "./Control.css";
 import { usePagination } from "../Common/hooks.js";
@@ -79,10 +80,12 @@ function Warehouse() {
 
   return (
     <>
-      <Navbar expand="lg" className="custom-navbar">
-        <Container>
+      <Navbar expand="lg" className="custom-navbar ">
+        <Container style={{margin : 0}}>
           <Navbar.Brand href="#home" className="custom-brand">
-            Control
+          <Navbar.Brand href="#home" >
+            <Image src="./images/logo.png" style={{height : "50px",width : "50px",objectFit : "cover"}}  />
+          </Navbar.Brand>
           </Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="#overview">Tổng quan</Nav.Link>
@@ -116,106 +119,8 @@ function Warehouse() {
 
       <Container fluid>
         <Row>
-          <Col md={2} className="sidebar">
-            <div>
-              <InputGroup className="mb-7">
-                <FormControl
-                  placeholder="MÃ HÀNG HÓA, TÊN"
-                  aria-label="MÃ HÀNG HÓA, TÊN"
-                />
-
-                <Button variant="outline-secondary" id="button-addon2">
-                  Search
-                </Button>
-              </InputGroup>
-              <InputGroup className="mb-5">
-                <FormControl
-                  placeholder="TÊN NHÀ CUNG CẤP"
-                  aria-label="MÃ HÀNG HÓA, TÊN"
-                />
-                <Button variant="outline-secondary" id="button-addon2">
-                  Search
-                </Button>
-              </InputGroup>
-
-              <div className="filter-section">
-                <h5 className="text-decoration-underline">
-                  Lọc theo loại hàng hóa
-                </h5>
-                <Form.Check
-                  label="COFFEE BEAN"
-                  name="group1"
-                  type="checkbox"
-                  id="checkbox1"
-                  className="mb-1"
-                />
-                <Form.Check
-                  label="COFFEE BEAN"
-                  name="group1"
-                  type="checkbox"
-                  id="checkbox2"
-                  className="mb-1"
-                />
-                <Form.Check
-                  label="COFFEE BEAN"
-                  name="group1"
-                  type="checkbox"
-                  id="checkbox3"
-                  className="mb-1"
-                />
-                <Form.Check
-                  label="COFFEE BEAN"
-                  name="group1"
-                  type="checkbox"
-                  id="checkbox4"
-                  className="mb-1"
-                />
-              </div>
-
-              <div className="filter-section mt-3">
-                <h5 className="text-decoration-underline">
-                  Lọc theo loại nhóm
-                </h5>
-                <ListGroup variant="flush">
-                  <ListGroup.Item action variant="light">
-                    TẤT CẢ
-                  </ListGroup.Item>
-                  <ListGroup.Item action variant="light">
-                    COFFEE BEAN
-                  </ListGroup.Item>
-                  <ListGroup.Item action variant="light">
-                    MILK TEA
-                  </ListGroup.Item>
-                  <ListGroup.Item action variant="light">
-                    COFFEE
-                  </ListGroup.Item>
-                  <ListGroup.Item action variant="light">
-                    CAKE
-                  </ListGroup.Item>
-                  {/* Add more ListGroup.Items as needed */}
-                </ListGroup>
-              </div>
-
-              <div className="filter-section mt-3">
-                <h5 className="text-decoration-underline">Lọc theo tồn kho</h5>
-                <Form.Check
-                  label="CÒN HÀNG"
-                  name="group2"
-                  type="checkbox"
-                  id="checkbox5"
-                  className="mb-1"
-                />
-                <Form.Check
-                  label="HẾT HÀNG"
-                  name="group2"
-                  type="checkbox"
-                  id="checkbox6"
-                  className="mb-1"
-                />
-              </div>
-            </div>
-          </Col>
-          <Col xs={8}>
+          
+          <Col xs={12}>
             <Row className="justify-content-center align-items-center">
               <Container fluid >
                 <Row className="justify-content-center align-items-center">
