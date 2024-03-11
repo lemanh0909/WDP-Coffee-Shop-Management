bd866b5f5a72d83b31c6347a991a56ebe875033e
lemanh0909 <manhldhe160520@fpt.edu.vn>
2024-03-11 19:00:11 +0700

    Order Fe

--- frontend/src/pages/Order/Order.jsx
+++ frontend/src/pages/Order/Order.jsx
@@ -1,170 +1,253 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Card, Container, Button } from 'react-bootstrap';
-// import { StyledTable, StyledPagination } from './managerOrderStyles';
+import { Container } from 'react-bootstrap';
 import CommonNavbar from '../Common/navbar';
+import CloseButton from 'react-bootstrap/CloseButton';
+import OrderSummary from './OrderSummary';
+import OrderDisplay from './OrderDisplay';
+import ProductVariantDisplay from './ProductVariantDisplay';
 
 const Order = () => {
-  const [categories, setCategories] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState();
-  const [products, setProducts] = useState();
+  const [productVariant, setProductVariant] = useState([]);
+  const [shopId, setShopId] = useState('65d749ea36f223b9f7040016');
+  const [userId, setUserId] = useState('65d749ea36f223b9f7040014');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const [warehouse, setWarehouse] = useState([]);
+  const [orderMessage, setOrderMessage] = useState(null);
+  const [lowQuantityItems, setLowQuantityItems] = useState([]);
+  const [isLowQuantityNotificationVisible, setIsLowQuantityNotificationVisible] = useState(true);
+  const [showOrderSummary, setShowOrderSummary] = useState(false);
+  const [customerPayment, setCustomerPayment] = useState(0);
+  const [refundAmount, setRefundAmount] = useState(0);
+  const [paymentMethod, setPaymentMethod] = useState('Cash');
+  const [refundAmountChange, setRefundAmountChange] = useState(false);
+  const [creatOrderError, setCreatOrderError] = useState(null);
+  const [orderErrorVisible, setOrderErrorVisible] = useState(false);
 
   useEffect(() => {
-    // Gọi API để lấy danh sách category khi component được mount
-    axios.get('http://localhost:5000/api/v1/category/getAll')
+    axios.get(`http://localhost:5000/api/v1/productVariant/${shopId}/getAllProductVariantsInShop`)
       .then(response => {
-        setCategories(response.data.data.data);
+        setProductVariant(response.data.data.allProductVariants);
+      })
+      .catch(error => console.error('Error fetching categories:', error));
+
+    axios.get(`http://localhost:5000/api/v1/warehouse/${shopId}/getAllWarehousesInShop`)
+      .then(response => {
+        setWarehouse(response.data.data);
+        const lowQuantityItems = response.data.data.filter(item => item.quantity < 10);
+        setLowQuantityItems(lowQuantityItems);
       })
       .catch(error => console.error('Error fetching categories:', error));
   }, []);
 
-  const handleCategoryClick = (categoryId) => {
-    // Gọi API để lấy danh sách category khi component được mount
-    setProducts(null)
-    axios.get(`http://localhost:5000/api/v1/category/${categoryId}/getDetail`)
-      .then(response => {
-        const productPromises = response.data.data.data.products.map((productId) => {
-          // Return the promise for each product request
-          return axios.get(`http://localhost:5000/api/v1/product/${productId}/getProductById`)
-            .then(response => response.data.data);
-        });
+  useEffect(() => {
+    // Tính tổng số lượng sản phẩm khi danh sách thay đổi
+    const newTotalQuantity = selectedProducts.reduce((total, product) => total + product.quantity, 0);
+    setTotalQuantity(newTotalQuantity);
+  }, [selectedProducts]);
 
-        // Use Promise.all to wait for all product requests to complete
-        Promise.all(productPromises)
-          .then(productsData => {
-            // Update state with both the selected category and the associated products
-            setSelectedCategory({
-              ...response.data.data.data,
-              products: productsData,
-            });
-          })
-          .catch(error => console.error('Error fetching products:', error));
-      })
-      .catch(error => console.error('Error fetching categories:', error));
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
   };
 
-  const handleProductClick = (productId) => {
-    // Gọi API để lấy danh sách category khi component được mount
-    axios.get(`http://localhost:5000/api/v1/product/${productId}/getProductById`)
-      .then(response => {
-        const sortedProductVariants = response.data.data.productVariant.sort((a, b) => {
-          // Sắp xếp theo giá từ thấp đến cao, bạn có thể điều chỉnh nếu cần
-          return a.price - b.price;
-        });
+  const handleSelect = (selectedProduct) => {
+    const existingProductIndex = selectedProducts.findIndex(
+      (product) => product._id === selectedProduct._id
+    );
 
-        const productPromises = sortedProductVariants.map((productVariant) => {
-          // Return the promise for each product request
-          return axios.get(`http://localhost:5000/api/v1/productVariant/${productVariant._id}/getProductVariant`)
-            .then(response => response.data.data);
-        });
-
-        // Use Promise.all to wait for all product requests to complete
-        Promise.all(productPromises)
-          .then(productsData => {
-            // Update state with both the selected category and the associated products
-            setProducts({
-              ...response.data.data,
-              productsData,
-            });
-          })
-          .catch(error => console.error('Error fetching products:', error));
-      })
-      .catch(error => console.error('Error fetching categories:', error));
+    if (existingProductIndex !== -1) {
+      const updatedProducts = [...selectedProducts];
+      updatedProducts[existingProductIndex].quantity += 1;
+      setSelectedProducts(updatedProducts);
+    } else {
+      setSelectedProducts([...selectedProducts, { ...selectedProduct, quantity: 1 }]);
+    }
   };
 
-
-  const ProductVariantDisplay = () => {
-    return (
-      <div className="">
-        <p>Selected Product Variant: {products.name}</p>
-        <h4>Products:</h4>
-        <ul className='flex justify-around'>
-          {console.log(products?.productVariant)}
-          {products?.productVariant.map(productVariant => (
-            <li key={productVariant._id}>
-              <div>
-                <p>Name: {productVariant.name}</p>
-                <p>Size: {productVariant.size}</p>
-                <p>Price: {productVariant.price}</p>
-                <Button
-                  variant="primary"
-                // onClick={() => handleProductVariantSelect(productVariant._id)}
-                >
-                  Select
-                </Button>
-                <Button
-                  variant="danger"
-                // onClick={() => handleProductVariantCancel(productVariant._id)}
-                >
-                  Cancel
-                </Button>
-              </div>
-            </li>
-          ))}
-        </ul>
-
-      </div>
+  const handleCancel = (selectedProduct) => {
+    setOrderMessage(null)
+    const existingProductIndex = selectedProducts.findIndex(
+      (product) => product._id === selectedProduct._id
     );
-  }
 
-  const ProductDisplay = () => {
-    return (
-      <div className="">
-        <p>Selected Product: {selectedCategory.name}</p>
-        <h4>Products:</h4>
-        <ul className='flex justify-around'>
-          {selectedCategory?.products.map(product => (
-            <li key={product._id}>
-              <Button
-                variant={products?._id === product._id ? 'primary' : 'success'}
-                onClick={() => handleProductClick(product._id)}
-              >
-                {product.name}
-              </Button>
-            </li>
-          ))}
-        </ul>
-        {products && <ProductVariantDisplay />}
-      </div>
-    );
-  }
+    if (existingProductIndex !== -1) {
+      const updatedProducts = [...selectedProducts];
+      const currentQuantity = updatedProducts[existingProductIndex].quantity;
 
-  const CategoryDisplay = () => {
-    // Hiển thị danh sách category dưới dạng các button
-    return (
-      <Card className="w-1/2">
-        <div>
-          <h3>Categories</h3>
-          <ul className='flex justify-around'>
-            {categories.map(category => (
-              <li key={category._id}>
-                <Button
-                  variant={selectedCategory?._id === category._id ? 'primary' : 'success'}
-                  onClick={() => handleCategoryClick(category._id)}
-                >
-                  {category.name}
-                </Button>
-              </li>
-            ))}
-          </ul>
-        </div>
+      if (currentQuantity > 1) {
+        updatedProducts[existingProductIndex].quantity -= 1;
+      } else {
+        updatedProducts.splice(existingProductIndex, 1);
+      }
 
-        <div>
-          {selectedCategory && <ProductDisplay />}
-        </div>
+      setSelectedProducts(updatedProducts);
+    }
+  };
 
-      </Card>
-    );
+  const handleChangeQuantity = (index, newQuantity) => {
+    const updatedProducts = [...selectedProducts];
+    updatedProducts[index].quantity = parseInt(newQuantity, 10) || 1;
+    setSelectedProducts(updatedProducts);
+  };
+
+  const handleCloseLowQuantityNotification = () => {
+    setIsLowQuantityNotificationVisible(false);
+  };
+
+  const handleRemove = (index) => {
+    const updatedProducts = [...selectedProducts];
+    updatedProducts.splice(index, 1);
+    setSelectedProducts(updatedProducts);
+    setOrderMessage(null);
+  };
+
+  const handleCreateOrder = () => {
+    const warehouseCopy = warehouse.map(item => ({ ...item }));
+    setOrderMessage(null)
+    if (selectedProducts.length == 0) {
+      setOrderMessage('Hóa đơn trống!');
+      return;
+    }
+    for (const product of selectedProducts) {
+      for (const item of product.recipe) {
+        // Tìm phần tử trong warehouse giống với item
+        const matchingWarehouseItem = warehouseCopy.find(
+          warehouseItem => warehouseItem._id === item.warehouse._id);
+
+        if (matchingWarehouseItem) {
+          matchingWarehouseItem.quantity = matchingWarehouseItem.quantity - item.require * product.quantity;
+          if (matchingWarehouseItem.quantity < 0) {
+            console.log(matchingWarehouseItem.name, 'not enough:');
+            setOrderMessage(`Không đủ nguyên liệu để tạo ${product.name}.`)
+            return;
+          }
+        }
+      }
+    }
+    setShowOrderSummary(true);
+  };
+
+  const handleGoBack = () => {
+    setShowOrderSummary(false);
+  };
+
+  const calculateRefundAmount = () => {
+    const paidAmount = parseFloat(customerPayment) || 0;
+    const totalAmount = calculateTotalPrice();
+
+    // Đảm bảo paidAmount là một số hợp lệ
+    if (!isNaN(paidAmount) && paidAmount !== 0) {
+      return Math.max(0, paidAmount - totalAmount);
+    }
+
+    return 0;
+  };
+
+  const calculateTotalPrice = () => {
+    return selectedProducts.reduce((total, product) => {
+      return total + product.quantity * product.price;
+    }, 0);
+  };
+
+  const handlePaymentChange = (e) => {
+    const paidAmount = e.target.value;
+    setCustomerPayment(paidAmount);
+    setRefundAmountChange(true);
+  };
+
+  useEffect(() => {
+    if (refundAmountChange) {
+      const refund = calculateRefundAmount();
+      setRefundAmount(refund);
+      setRefundAmountChange(false);
+    }
+  }, [refundAmountChange, calculateRefundAmount]);
+
+  const handleConfirmOrder = () => {
+    const customerPayInt = parseInt(customerPayment);
+    const totalPrice = calculateTotalPrice();
+    if (customerPayInt >= totalPrice) {
+      const order = {
+        shopId: shopId,
+        products: selectedProducts.map(product => ({
+          productId: product._id,
+          quantity: parseInt(product.quantity),
+        })),
+        userId: userId,
+        paymentMethod: paymentMethod,
+        customerPay: customerPayInt,
+        refund: refundAmount,
+      };
+      console.log(order);
+      axios.post('http://localhost:5000/api/v1/order/create', order)
+        .then(response => {
+          console.log("Order created");
+          setOrderErrorVisible(false);
+          setCustomerPayment(0);
+          setPaymentMethod('Cash');
+          alert('Đơn hàng đã được tạo thành công!');
+          window.location.reload();
+        })
+        .catch(error => {
+          console.error('Error creating order:', error);
+        });
+    } else {
+      setOrderErrorVisible(true);
+      setCreatOrderError(`Tiền khách trả ít hơn tổng giá!`);
+    }
   };
 
   return (
     <>
       <CommonNavbar />
-      <Container className="mt-4 flex">
-        <Card className='w-1/2'>
-          Hello
-        </Card>
-        <CategoryDisplay />
+      {isLowQuantityNotificationVisible && lowQuantityItems.length > 0 && (
+        <div className="alert alert-warning position-relative">
+          <CloseButton onClick={handleCloseLowQuantityNotification} className="position-absolute top-0 end-0 m-2" />
+          Nguyên liệu có số lượng thấp:
+          <ul className='flex gap-2'>
+            {lowQuantityItems.map(item => (
+              <li key={item._id}>{item.name}: {item.quantity} {item.unit}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+      <Container className="flex justify-center gap-4 mt-4">
+        {showOrderSummary ? (
+          <OrderSummary
+            handleGoBack={handleGoBack}
+            totalQuantity={totalQuantity}
+            calculateTotalPrice={calculateTotalPrice}
+            paymentMethod={paymentMethod}
+            setPaymentMethod={setPaymentMethod}
+            customerPayment={customerPayment}
+            handlePaymentChange={handlePaymentChange}
+            refundAmount={refundAmount}
+            handleConfirmOrder={handleConfirmOrder}
+            orderErrorVisible={orderErrorVisible}
+            creatOrderError={creatOrderError}
+          />
+        ) : (
+          <>
+            <OrderDisplay
+              totalQuantity={totalQuantity}
+              selectedProducts={selectedProducts}
+              handleChangeQuantity={handleChangeQuantity}
+              handleRemove={handleRemove}
+              handleCreateOrder={handleCreateOrder}
+              orderMessage={orderMessage}
+            />
+            <ProductVariantDisplay
+              productVariant={productVariant}
+              handleSearch={handleSearch}
+              searchTerm={searchTerm}
+              handleSelect={handleSelect}
+              handleCancel={handleCancel}
+            />
+          </>
+        )}
       </Container>
     </>
   );
