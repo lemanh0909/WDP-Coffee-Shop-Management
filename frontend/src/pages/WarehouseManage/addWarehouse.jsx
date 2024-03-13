3572ead879bf942c3adbafb755c6fe34330fb178
wualnupp <116285769+hongquan28032002@users.noreply.github.com>
2024-03-14 02:09:17 +0700

    warehouse 90%

---
+++ frontend/src/pages/WarehouseManage/addWarehouse.jsx
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import axios from "axios";
+
+const AddWarehouseModal = ({ userId, show, onHide, handleAddWarehouse }) => {
+    const [formData, setFormData] = useState({
+        managerId: userId,
+        name: "",
+        quantity: "",
+        unit: "",
+        image: "",
+    });
+
+    const handleChange = async (e) => {
+        e.persist();
+        try {
+            const { name, value } = e.target;
+
+            if (name === 'quantity' && (!Number.isInteger(Number(value)) || Number(value) < 0)) {
+                return;
+            }
+
+            setFormData((prevData) => ({ ...prevData, [name]: value }));
+        } catch (error) {
+            console.error("Error handling change:", error);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!Number.isInteger(Number(formData.quantity)) || Number(formData.quantity) < 0) {
+            return;
+        }
+
+        try {
+            const response = await axios.post(
+                "http://localhost:5000/api/v1/warehouse/create",
+                formData
+            );
+
+            if (response.data.isSuccess) {
+                onHide();
+                handleAddWarehouse(response.data.data);
+                setFormData({
+                    managerId: userId,
+                    name: "",
+                    quantity: "",
+                    unit: "",
+                    image: "",
+                });
+            } else {
+                console.error("Error:", response.data.message);
+            }
+        } catch (error) {
+            console.error("Unexpected error:", error);
+        }
+    };
+
+    return (
+        <Modal show={show} onHide={onHide} animation={false}>
+            <Modal.Header closeButton>
+                <Modal.Title>Thêm sản phẩm</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formName">
+                        <Form.Label>Tên sản phẩm:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="name"
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formQuantity">
+                        <Form.Label>Số lượng:</Form.Label>
+                        <Form.Control
+                            type="number"
+                            name="quantity"
+                            value={formData.quantity}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formUnit">
+                        <Form.Label>Đơn vị:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="unit"
+                            value={formData.unit}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formImage">
+                        <Form.Label>Đường dẫn ảnh:</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="image"
+                            value={formData.image}
+                            onChange={handleChange}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Thêm sản phẩm
+                    </Button>
+                </Form>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
+export default AddWarehouseModal;
