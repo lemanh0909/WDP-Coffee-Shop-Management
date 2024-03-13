9b89ac8ef4120e9f95709b1eb8a96e69174bbd1f
nguyen ha trang <trangnthhe163554@fpt.edu.vn>
2024-03-13 22:43:48 +0700

    Merge branch 'main' of https://github.com/lemanh0909/WDP-Coffee-Shop-Management into TrangNTH

--- frontend/src/pages/ViewOrder/viewOrderDetail.jsx
+++ frontend/src/pages/ViewOrder/viewOrderDetail.jsx
@@ -1,58 +1,65 @@
 
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+
+import {Container,Row,Col,Card, Pagination, Table, Button} from "react-bootstrap";
 import "./viewOrderDetail.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import CommonNavbar from "../Common/navbar.jsx";
+import CommonNavbar from "../Common/navbar.jsx"
+import Sidebar from "../Common/sidebar.jsx"
+import React, {useEffect } from "react";
+import { usePagination } from "../Common/hooks.js";
 
-function ViewOrderDetail() {
-  const { orderId } = useParams(); 
-  const [order, setOrder] = useState(null); 
+function ViewDetail() {
 
-  
-  const fetchOrderDetail = async () => {
-    try {
-      
-      const response = await fetch(`api/orders/${orderId}`);
-      const data = await response.json();
-      setOrder(data); // Lưu thông tin đơn hàng vào state
-    } catch (error) {
-      console.error("Error fetching order detail:", error);
-    }
-  };
+    useEffect(() => {
+        const checkboxes = document.querySelectorAll(
+            '.filter-section input[type="checkbox"]'
+        );
 
-  
-  useEffect(() => {
-    fetchOrderDetail();
-  }, []);
+        checkboxes.forEach((checkbox) => {
+            checkbox.addEventListener("click", function () {
+                if (this.checked) {
+                    this.parentElement.classList.add("selected");
+                } else {
+                    this.parentElement.classList.remove("selected");
+                }
+            });
+        });
+    }, []);
 
-  return (
-    <>
-      <CommonNavbar />
-      <Container fluid>
-        <Row className="title justify-content-center">
-          <Col md={8} className="text-center">
-            <h2>Chi Tiết Đơn Hàng</h2>
-          </Col>
-        </Row>
+    return (
+        <>
+            <CommonNavbar />
+            <Row md={5} className="title">
+                <Col md={4}>
+                    <h2>Chi tiết đơn hàng</h2>
+                </Col><Col md={4} />
 
-        {order && (
-          <Row>
-            <Col md={12}>
-              <p>Mã đơn hàng: {order.id}</p>
-              <p>Ngày bán: {order.date}</p>
-              <p>Người bán: {order.seller}</p>
-              <p>Tổng giá: {order.total}</p>
-              <p>Khách trả: {order.paidByCustomer}</p>
-              <p>Trạng thái: {order.status}</p>
-              <p>Hình thức thanh toán: {order.paymentMethod}</p>
-            </Col>
-          </Row>
-        )}
-      </Container>
-    </>
-  );
+                <Col md={4} className="button-container">
+                    <button type="button" className="btn btn-primary add-btn">
+                        <i class="fa-solid fa-plus"></i> Thêm danh mục
+                    </button>
+                    <button type="button" className="btn btn-primary">
+                        <i class="fa-solid fa-file-export"></i>
+                        Xuất ra file
+                    </button>
+                </Col>
+
+            </Row>
+
+            <Container fluid>
+                <Row>
+                    <Sidebar />
+                    <Col xs={9}>
+                        <Row>
+                            {/* code here */}
+                        </Row>
+
+                        
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
 }
 
-export default ViewOrderDetail;
+export default ViewDetail;
