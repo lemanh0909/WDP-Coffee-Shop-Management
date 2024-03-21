1785f4c42da3d2c7cbac0ecd911dbe5be1d2bdd9
lemanh0909 <manhldhe160520@fpt.edu.vn>
2024-03-21 11:08:57 +0700

    FE Product

--- frontend/src/pages/ProductManage/productmanage.jsx
+++ frontend/src/pages/ProductManage/productmanage.jsx
@@ -17,15 +17,43 @@ function ProductManage() {
     usePagination(products, itemsPerPage);
   const [showModal, setShowModal] = useState(false);
   const [showDetailsTable, setShowDetailsTable] = useState(false);
-  const handleShowModal = () => setShowModal(true);
+
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
   const [productIdToDelete, setProductIdToDelete] = useState(null);
   const [showUpdateModal, setShowUpdateModal] = useState(false);
   const [productIdToUpdate, setProductIdToUpdate] = useState(null);
+  const [warehouse, setWarehouse] = useState([]);
+  const [selectedProductToUpdate, setSelectedProductToUpdate] = useState(null);
+
+const getWarehouse = () => {
+      // Lấy userId và shopId từ local storage
+      const userDataString = localStorage.getItem('userData');
+      if (!userDataString) {
+        throw new Error('User data not found in localStorage.');
+      }
+      const userData = JSON.parse(userDataString);
+      const shopId = userData.shopId;
+      const userId = userData.userID;
+      axios
+        .get(`http://localhost:5000/api/v1/warehouse/${shopId}/getAllWarehousesInShop`)
+        .then((response) => {
+          setWarehouse(response.data.data);
+        })
+        .catch((error) => {
+          console.error("Error getting warehouse:", error);
+        });
+}
+
+  const handleShowModal = () => {
+    getWarehouse();
+    setShowModal(true);
+  };
 
 
-  const handleShowUpdateModal = (productId) => {
-    setProductIdToUpdate(productId);
+
+  const handleShowUpdateModal = (product) => {
+    setSelectedProductToUpdate(product);
+    getWarehouse();
     setShowUpdateModal(true);
   };
 
@@ -36,7 +64,10 @@ function ProductManage() {
     setShowUpdateModal(false);
     setProductIdToUpdate(null);
   };
-
+  const handleUpdateSuccess = () => {
+    fetchProducts(); // Cập nhật danh sách sản phẩm sau khi cập nhật thành công
+    handleCloseUpdateModal(); // Đóng modal cập nhật sản phẩm
+  };
   const handleCloseModal = () => {
     setShowModal(false);
     setShowDetailsTable(false);
@@ -53,9 +84,7 @@ function ProductManage() {
 
   const handleDelete = () => {
     axios
-      .delete("http://localhost:5000/api/v1/product/delete", {
-        data: { productId: productIdToDelete },
-      })
+      .delete(`http://localhost:5000/api/v1/product/${productIdToDelete}/delete`)
       .then((response) => {
         console.log("Product deleted successfully");
         fetchProducts();
@@ -91,9 +120,9 @@ function ProductManage() {
       setShowDetailsTable(!showDetailsTable);
     } else {
       axios
-        .get(`http://localhost:5000/api/v1/product/${productId}/getProductByIdTotalVariant`)
+        .get(`http://localhost:5000/api/v1/product/${productId}/getProductVariant`)
         .then((response) => {
-          setSelectedProduct(response.data.data);
+          setSelectedProduct(response.data.data.productVariant);
           setShowDetailsTable(true);
         })
         .catch((error) => {
@@ -126,6 +155,19 @@ function ProductManage() {
   }, []);
 
 
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showImageModal, setShowImageModal] = useState(false);
+
+  // Hàm để đóng modal ảnh
+  const handleCloseImageModal = () => {
+    setShowImageModal(false);
+  };
+  // Hàm để mở modal và hiển thị ảnh được click
+  const handleImageClick = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setShowImageModal(true);
+  };
+
   return (
     <>
       <CommonNavbar />
@@ -148,32 +190,31 @@ function ProductManage() {
                     className="add-btn btn-danger"
                     onClick={handleShowModal}
                   >
-                    <i className="fa-solid fa-plus"></i> Thêm sản phẩm
-                  </Button>
-                  <Button variant="primary" className="btn-secondary">
-                    <i className="fa-solid fa-file-export"></i> Biến thể
+                    <i className="fa-solid fa-plus"></i> Add Product
                   </Button>
                 </div>
               </Col>
             </Row>
-            <Row>
+            <Row className="container-table table">
               <Col xs={12}>
                 <Row>
-                  <Table striped bordered hover>
+                  <Table striped bordered hover >
                     <thead>
                       <tr>
-                        <th>
-                          Mã hàng hóa
+                        <th className="text-center">
+                          Product Id
                         </th>
-                        <th>
-                          Tên hàng hóa
+                        <th className="text-center">
+                          Product Name
                         </th>
-                        <th>Category</th>
-                        <th></th>
-                        <th></th>
+                        <th className="text-center">Category</th>
+                        <th className="text-center">Size</th>
+                        <th className="text-center">Price</th>
+                        <th className="text-center">Details</th>
+                        <th className="text-center">Action</th>
                       </tr>
                     </thead>
-                    <tbody>
+                    <tbody >
                       {paginatedItems.length === 0 ? (
                         <tr>
                           <td colSpan="5" className="text-center">No data to present!</td>
@@ -181,17 +222,20 @@ function ProductManage() {
                       ) : (
                         paginatedItems.map((item) => (
                           <React.Fragment key={item._id}>
-                            <tr>
-                              <td>{item._id}</td>
-                              <td style={{ color: "#BB2649", fontWeight: "bold" }}>{item.name}</td>
-                              <td>{item.category.name}</td>
-                              <td>
+                            <tr >
+                              <td className="text-center">{item._id}</td>
+                              <td className="text-center" style={{ color: "#BB2649", fontWeight: "bold" }}>{item.name}</td>
+                              <td className="text-center">{item.category.name}</td>
+                              <td className="text-center">{item.size}</td>
+                              <td className="text-center">{item.price}</td>
+                              <td className="text-center justify-center">
                                 <Button onClick={() => showDetails(item._id)}>
-                                  Xem chi tiết
+                                  View Details
                                 </Button>
                               </td>
-                              <td>
-                                <Button variant="primary" className="edit-btn" onClick={() => handleShowUpdateModal(item._id)}>
+                              <td className="text-center">
+                                <Button variant="primary" className="edit-btn"
+                                  onClick={() => handleShowUpdateModal(item)}>
                                   <i className="fa-solid fa-pen-to-square"></i> Update
                                 </Button>
                                 <Button variant="danger" onClick={() => handleShowConfirmationModal(item._id)}>
@@ -208,32 +252,69 @@ function ProductManage() {
                                         <Table bordered>
                                           <tbody>
                                             <tr>
-                                              <td className="field w-2/5">Tên:</td>
+                                              <td className="field w-2/5">Product ID:</td>
+                                              <td>{selectedProduct._id}</td>
+                                            </tr>
+                                            <tr>
+                                              <td className="field w-2/5">Product name:</td>
                                               <td>{selectedProduct.name}</td>
                                             </tr>
                                             <tr>
-                                              <td className="field">Mô tả:</td>
+                                              <td className="field">Description:</td>
                                               <td>{selectedProduct.description}</td>
                                             </tr>
                                             <tr>
-                                              <td className="field">Hình ảnh:</td>
-                                              <td>
-                                                <div className="flex flex-wrap gap-3 justify-center">
+                                              <td className="field w-2/5">Product Size:</td>
+                                              <td>{selectedProduct.size}</td>
+                                            </tr>
+                                            <tr>
+                                              <td className="field w-2/5">Product price:</td>
+                                              <td>{selectedProduct.price}</td>
+                                            </tr>
+                                            <tr>
+                                              <td className="field">Image:</td>
+                                              <td className="text-center">
+                                                <div className="grid grid-cols-2 gap-4">
                                                   {selectedProduct.image.map((imageUrl, index) => (
-                                                    <img
-                                                      key={index}
-                                                      className="w-1/4 h-1/4"
-                                                      src={imageUrl}
-                                                      alt={`Product ${index + 1}`}
-                                                    />
+                                                    <div key={index}
+                                                      className=" border border-gray-300 rounded-md 
+                                                      p-2 flex justify-center items-center"
+                                                      onClick={() => handleImageClick(imageUrl)}>
+                                                      <img
+                                                        className="w-full h-auto"
+                                                        src={imageUrl}
+                                                        alt={`Product ${index + 1}`}
+                                                      />
+                                                    </div>
                                                   ))}
                                                 </div>
                                               </td>
                                             </tr>
                                             <tr>
-                                              <td className="field">Số lượng biến thể:</td>
+                                              <td className="field">Recipe:</td>
                                               <td>
-                                                {selectedProduct.productVariant}
+                                                <Table bordered>
+                                                  <thead>
+                                                    <tr>
+                                                      <th>STT</th>
+                                                      <th>ID</th>
+                                                      <th>Name</th>
+                                                      <th>Require</th>
+                                                      <th>Unit</th>
+                                                    </tr>
+                                                  </thead>
+                                                  <tbody>
+                                                    {selectedProduct.recipe.map((product, idx) => (
+                                                      <tr key={idx}>
+                                                        <td>{idx + 1}</td>
+                                                        <td>{product.warehouse._id}</td>
+                                                        <td>{product.warehouse.name}</td>
+                                                        <td>{product.require}</td>
+                                                        <td>{product.warehouse.unit}</td>
+                                                      </tr>
+                                                    ))}
+                                                  </tbody>
+                                                </Table>
                                               </td>
                                             </tr>
                                           </tbody>
@@ -250,7 +331,7 @@ function ProductManage() {
                 </Row>
                 <Row>
                   <Col>
-                    <Pagination>
+                    <Pagination className="justify-center">
                       <Pagination.Prev
                         onClick={() => handlePageChange(activePage - 1)}
                         disabled={activePage === 1}
@@ -281,16 +362,23 @@ function ProductManage() {
         handleClose={handleCloseModal}
         onAddSuccess={handleAddSuccess}
         categories={categories}
+        warehouse={warehouse}
 
       />
 
+      {
+        selectedProductToUpdate != null &&
+        <UpdateProductModal
+          show={showUpdateModal}
+          handleClose={handleCloseUpdateModal}
+          productInfo={selectedProductToUpdate}
+          onUpdate={handleUpdateSuccess}
+          categories={categories}
+          warehouse={warehouse}
+        />
+      }
 
-      <UpdateProductModal
-        show={showUpdateModal}
-        handleClose={handleCloseUpdateModal}
-        productId={productIdToUpdate}
-        onUpdate={fetchProducts}
-      />
+
       <Modal show={showConfirmationModal} onHide={handleCloseConfirmationModal}>
         <Modal.Header closeButton>
           <Modal.Title>Xác nhận xoá sản phẩm</Modal.Title>
@@ -302,6 +390,21 @@ function ProductManage() {
           </Button>
           <Button variant="danger" onClick={handleDelete}>
             Xoá
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      {/* Modal xem ảnh */}
+      <Modal show={showImageModal} onHide={handleCloseImageModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Product Image</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img src={selectedImage} alt="Product" className="w-full" />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseImageModal}>
+            Đóng
           </Button>
         </Modal.Footer>
       </Modal>
