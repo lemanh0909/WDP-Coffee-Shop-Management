038920fdfb6766a35aa108fc36c88ebd6dee1df8
wualnupp <116285769+hongquan28032002@users.noreply.github.com>
2024-03-21 21:43:13 +0700

    ..

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -19,7 +19,7 @@ function Login() {
     const [nameError, setNameError] = useState('');
 
     const validateEmail = (email) => {
-        const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+        const regex = /^[^\s@]+@[^\s@]+\.[a-zA-Z]{2,}$/
         return regex.test(email);
     };
 
